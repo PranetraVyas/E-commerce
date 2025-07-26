42c8ddf1c358a2e6578928d5ad5d1273c05659f0
nilesh-catrak <pranetravyas3001@gmail.com>
2025-07-26 13:01:16 +0530

    added navbar borders

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+function Navbar(){
+    return(
+        <>
+        <div className="h-20 w-screen bg-amber-50 flex">
+            <div className="border-1 border-black h-20 w-50">
+
+            </div>
+            <div className="border-1 border-amber-400 h-20 w-200">
+
+            </div>
+            <div className="border-1 border-amber-950 h-20 w-140">
+
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Navbar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar"
+
+function Home(){
+    return (
+        <>
+        <div className="bg-gray-200 h-screen w-screen">
+        <Navbar/>
+        </div>
+        </>
+    )
+}
+
+export default Home
